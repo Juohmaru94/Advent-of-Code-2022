b7597d3f06e901e89aba3093c39d5617826c4d95
juohmaru94 <dimmoe@hotmail.com>
2022-12-06 09:03:33 +0200

    final answer

--- 2022/Day_3/day_3.py
+++ 2022/Day_3/day_3.py
@@ -1,8 +1,17 @@
 with open("day_3_input.txt") as f:
     data = [i for i in f.read().strip().split("\n")]
 
+values_upper = {chr(i): i - 38 for i in range(ord("A"), ord("A") + 26)}
+values_lower = {chr(i): i - 96 for i in range(ord("a"), ord("a") + 26)}
+values =  {**values_lower, **values_upper}
+
+count = 0
 
 for i in range(len(data)):
-  firstpart, secondpart = data[i][:len(data[i])//2], data[i][len(data[i])//2:]
-  set(firstpart)
-  set(secondpart)
+  a, b = data[i][:len(data[i])//2], data[i][len(data[i])//2:]
+  common = "".join(set(a).intersection(b))
+
+  for letter in common:
+    count += values[common]
+
+print(f"The answer to question 1 is: {count}")
