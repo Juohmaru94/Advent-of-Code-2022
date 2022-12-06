523c96840bb24772d5476ce4dfeed5a36b2b59a2
juohmaru94 <dimmoe@hotmail.com>
2022-12-06 16:34:04 +0200

    answer two complete

--- 2022/Day_4/day_4.py
+++ 2022/Day_4/day_4.py
@@ -7,4 +7,12 @@ for i in range(len(data)):
   if (int(a[0]) >= int(a[2])) and (int(a[1]) <= int(a[3])) or (int(a[0]) <= int(a[2])) and (int(a[1]) >= int(a[3])):
     count += 1
 
-print (f'Solution to question 1 is: {count}')
+print (f'Solution to question 1 is:{count}')
+
+count_two=0
+for i in range(len(data)):
+  a = data[i].replace("-"," ").replace(","," ").split(' ')
+  if (int(a[2]) <= int(a[0]) <= int(a[3])) or (int(a[2]) <= int(a[1]) <= int(a[3])) or (int(a[0]) <= int(a[2]) <= int(a[1])) or (int(a[0]) <= int(a[3]) <= int(a[1])):
+    count_two += 1
+
+print(f'Solution to question 1 is:{count_two}')
