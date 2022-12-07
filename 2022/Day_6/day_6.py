8ea8a725fe132ae450f60a5fd8488b028f1b94b5
juohmaru94 <dimmoe@hotmail.com>
2022-12-07 13:14:53 +0200

    completed

--- 2022/Day_6/day_6.py
+++ 2022/Day_6/day_6.py
@@ -3,13 +3,14 @@ with open("day_6_input.txt") as f:
     data.pop()
     data = [*data]
 
-def find_answer():
+def find_answer(num):
   for i in range(len(data)):
-    check_dataz = data[i:i+4]
+    check_dataz = data[i:i+num]
     check_data = set(check_dataz)
     if len(check_data) == len(check_dataz):
       return str(''.join(check_dataz))
 
 new_data = str(''.join(data))
-print (f"Answer to question 1 is: {new_data.rfind(find_answer()) + 4}")
+print (f"Answer to question 1 is: {new_data.rfind(find_answer(4)) + 4}")
+print (f"Answer to question 2 is: {new_data.rfind(find_answer(14)) + 14}")
 
